b1518e0602df5f4f1f917a47df7e893bc400b147
MarionM50 <marion@hey.com>
2024-02-08 12:02:32 +0000

    random AI responses + emojis

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+const aiResponses = [
+  "Pathetic human, your defeat was inevitable!",
+  "You're not even a worthy adversary, just a momentary annoyance.",
+  "How amusing, you believed victory was within your grasp.",
+  "You thought you could beat me? How cute",
+  "Not even worthy of my time...",
+  "Do you think randomness is your ally?",
+];
+
 const greetingMassage = {
-  title: "Welcome to the Rock, Paper, Scissors Game!",
+  title: "👋 Welcome to the Rock, Paper, Scissors Game!",
   intro:
-    "This game is a very simple, yet exciting game that pits two opponents against each other to determine the ultimate victor.",
+    "This is a very simple, yet exciting game that pits two opponents against each other to determine the ultimate victor.🏆",
 };
 
 const gameRules = {
-  title: "The Rules are straghtforward:",
-  line1: "Rocks beats Scissors",
-  line2: "Scissors beats Paper",
-  line3: "Paper beats Rock",
+  title: "The Rules are straightforward:",
+  line1: "→ Rock beats Scissors",
+  line2: "→ Scissors beats Paper",
+  line3: "→ Paper beats Rock",
   line4:
-    "Can you defeat the evil AI in this epic battle?You will play 5 rounds,at the end who get more points is the winner!!!",
+    "Can you defeat the 😈evil AI in this epic battle? You will play 5 rounds,at the end who get more points is the winner!!!",
   line5:
     "(I will post the rules on the console as well just in case you forget.)",
 };
 
-const initialQuestion = "Choose: rock, paper or scissors ?";
+const initialQuestion = "👉 Choose: rock, paper or scissors ?";
 const wrongInput = "Wrong Input!";
-const computerVictory = " Bad ending. Victory goes to the Evil AI!";
-const playerVictory = "What a Epic Game, You are the Victor!";
+const computerVictory = " Bad ending. Victory goes to the 😈Evil AI!";
+const playerVictory = "What a Epic Game, You are the 🏆Victor!";
 const draw = "After a close game You tied the Evil AI!";
 
 const nOfRounds = 5;
@@ -29,7 +38,12 @@ let computerScore = 0;
 const showMessage = (message) => {
   console.log(message);
   alert(message);
-}
+};
+
+const getRandomAiResponses = () => {
+  let response = "😈 You Lose! \n";
+  return response + aiResponses[Math.floor(Math.random() * aiResponses.length)];
+};
 
 const incrementPlayerScore = () => playerScore++;
 
@@ -38,29 +52,27 @@ const incrementComputerScore = () => computerScore++;
 const resetScores = () => {
   playerScore = 0;
   computerScore = 0;
-}
+};
 
 function determineWinner() {
   if (playerScore > computerScore) {
     showMessage(playerVictory);
-  }
-  else if(computerScore > playerScore) {
+  } else if (computerScore > playerScore) {
     showMessage(computerVictory);
-  }
-  else {
+  } else {
     showMessage(draw);
   }
 }
 
 function isPlayerReady() {
   try {
-    let response = prompt("Are you ready to play? (yes/no)");
+    let response = prompt("👉 Are you ready to play? (yes/no)");
     if (response === null) {
-      throw new Error("See you next time!!!");
+      throw new Error("👋 See you next time!!!");
     } else {
       while (response !== "yes" && response !== "no") {
-        showMessage("Invalid response. Please enter 'yes' or 'no'.");
-        response = prompt("Are you ready to play? (yes/no)").toLowerCase();
+        showMessage("❌ Invalid response. Please enter 'yes' or 'no'.");
+        response = prompt("👉 Are you ready to play? (yes/no)").toLowerCase();
       }
       return response === "yes";
     }
@@ -74,7 +86,7 @@ function appStart() {
   showMessage(
     `${gameRules.title}\n${gameRules.line1}\n${gameRules.line2}\n${gameRules.line3}\n${gameRules.line4}`
   );
-  
+
   gameStart();
 }
 
@@ -82,13 +94,13 @@ function gameStart() {
   if (isPlayerReady()) {
     game();
   } else {
-    showMessage("Alright, maybe next time. Goodbye!");
+    showMessage("Alright, maybe next time. Goodbye! 👋");
   }
 }
 
 function game() {
   // Play the game
-  showMessage("Game start");
+  showMessage("🚦 Game start");
   Rounds();
   determineWinner();
 
@@ -106,10 +118,11 @@ function computerPlay() {
   }
 }
 
-const isValidInput = (input) => input === 'rock' || input === 'paper' || input === 'scissors';
+const isValidInput = (input) =>
+  input === "rock" || input === "paper" || input === "scissors";
 
 function playerPlay(roundNumber) {
-  const roundCounterMessage = `ROUND ${roundNumber}`;
+  const roundCounterMessage = `🚩 ROUND ${roundNumber}`;
   let question = `${roundCounterMessage} \n${initialQuestion}`;
 
   while (true) {
@@ -124,13 +137,13 @@ function playerPlay(roundNumber) {
 }
 
 const showScores = (round) => {
-  let scoreMessage =`Computer: ${computerScore} - You: ${playerScore}`;
+  let scoreMessage = `Computer: ${computerScore} - You: ${playerScore}`;
   const roundsLeft = nOfRounds - round;
 
-  if(roundsLeft > 0) scoreMessage += `\n${roundsLeft} ROUNDS LEFT`;
+  if (roundsLeft > 0) scoreMessage += `\n${roundsLeft} ROUNDS LEFT`;
 
   showMessage(scoreMessage);
-}
+};
 
 function Rounds() {
   let round = 1;
@@ -143,23 +156,19 @@ function Rounds() {
 
 function whoWins(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "It's a Draw!";
-  }
-  else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "🤷‍♂️ It's a Draw!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
     incrementPlayerScore(); // rose added this line
-    return "You win! Rock beats Scissors!";
-  }
-  else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "🥳 You win! Rock beats Scissors!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     incrementPlayerScore(); // rose added this line
-    return "You Win! Scissors beats Paper";
-  }
-  else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "🥳 You Win! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     incrementPlayerScore(); // rose added this line
-    return "You Win! Paper beats Rock";
-  }
-  else {
+    return "🥳 You Win! Paper beats Rock";
+  } else {
     incrementComputerScore();
-    return "You Lose!";
+    return getRandomAiResponses();
   }
 }
 
@@ -167,22 +176,20 @@ function playGame(round) {
   let playerChoice = playerPlay(round);
   let computerChoice = computerPlay();
   let result = whoWins(playerChoice, computerChoice);
-  showMessage(`Player Choice: ${playerChoice}\nComputer Choice: ${computerChoice}\n${result}`);
+  showMessage(
+    `Player Choice: ${playerChoice}\nComputer Choice: ${computerChoice}\n${result}`
+  );
 }
 
 function reStartGame() {
-  let playerChoice = prompt("Do you want play again? (yes/no)")
+  let playerChoice = prompt("Do you want play again? (yes/no)");
   if (playerChoice == "yes") {
     console.clear();
     resetScores();
     gameStart();
-  }
-  else {
-    showMessage("Alright, see you soon!");
+  } else {
+    showMessage("👋 Alright, see you soon!");
   }
 }
 
 appStart();
-
-
-
